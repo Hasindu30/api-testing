829a7370cbd85070325cb2f4e469a1da7c40d65d
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-03-23 21:32:00 +0530

    update controller

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,40 @@
+// @desc Get goals 
+// @route Get/api/goals
+// @access Private 
+
+const getGoals = (req,res) =>{
+    res.status(200).json({message:'Get goals'})
+} 
+
+// @desc Set goals 
+// @route POST/api/goals
+// @access Private 
+
+const setGoal = (req,res) =>{
+    res.status(200).json({message:'set goals'})
+} 
+
+// @desc update goals 
+// @route PUT/api/goals/:id
+// @access Private 
+
+const updateGoal = (req,res) =>{
+    res.status(200).json({message:`update goals ${req.params.id}`})
+} 
+
+// @desc Delete goals 
+// @route DELETE/api/goals/:id
+// @access Private 
+
+const deleteGoal = (req,res) =>{
+    res.status(200).json({message:`delete goals ${req.params.id}`})
+} 
+
+
+module.exports ={
+    getGoals,
+    updateGoal,
+    deleteGoal,
+    setGoal,
+    
+}
